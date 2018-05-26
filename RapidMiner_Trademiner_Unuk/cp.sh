76d330cf9f0b31b86daa89091966e9070b78abf2
Ubuntu <ubuntu@ip-172-31-2-29.eu-west-1.compute.internal>
2018-05-26 20:15:17 +0000

    Fix for AMI

--- RapidMiner_Trademiner_Unuk/cp.sh
+++ RapidMiner_Trademiner_Unuk/cp.sh
@@ -1,2 +1,3 @@
-copy /y "../RapidMiner_Unuk/lib/plugins/rapidminer-Trademiner Extension-5.0.000.jar" "/home/nelson/rm/RapidMiner5\lib\plugins"
-call "/home/nelson/rm/RapidMiner5/scripts/RapidMinerGUIDebug"
+cp "../RapidMiner_Unuk/lib/plugins/rapidminer-Trademiner Extension-5.0.000.jar" /home/ubuntu/RapidMiner5/lib/plugins
+
+./home/ubuntu/RapidMiner5/scripts/RapidMinerGUI
